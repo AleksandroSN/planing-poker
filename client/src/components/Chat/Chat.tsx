6c5fffe6e0295a2c32ff45b7c80b06a6fdcf19c2
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-08 16:33:53 +0300

    feat: add base markup for Chat component

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,10 +1,27 @@
 import { FunctionComponent } from "react";
+import { User } from "../User/User";
 import "./chat.scss";
 
 interface ChatProps {
   message?: string;
 }
 
+const test = true;
+
 export const Chat: FunctionComponent<ChatProps> = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="chat">
+      <div className="chat__row">
+        <p className="chat__user-message test-light text-s">Hello All !:)</p>
+        <User
+          avatar="SA"
+          firstName="Semyon"
+          lastName="Aleks"
+          jobPosition="Junior front"
+          isYou={test}
+          isChat={test}
+        />
+      </div>
+    </div>
+  );
 };
