6752c980084d4ef5577e631ba69d09e3506501ca
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-10 15:46:20 +0300

    feat: add ability on CardsCover to select

--- client/src/features/LobbyPage/components/CardsCover/CardsCover.tsx
+++ client/src/features/LobbyPage/components/CardsCover/CardsCover.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 interface CardsCoverProps {
   imgNumber: number;
@@ -7,8 +7,20 @@ interface CardsCoverProps {
 export const CardsCover: FunctionComponent<CardsCoverProps> = ({
   imgNumber,
 }): JSX.Element => {
+  const [select, setSelect] = useState<boolean>(false);
+
+  const toggleSelect = () => {
+    setSelect((prev) => !prev);
+  };
+
   return (
-    <div className="game-cards__cover-item">
+    <div
+      className={
+        select ? "game-cards__cover-item selected" : "game-cards__cover-item"
+      }
+      role="none"
+      onClick={toggleSelect}
+    >
       <img src={`./img/cover${imgNumber}.png`} alt="card cover" />
     </div>
   );
