8e1542ea867b225f495e84140a1c4c8ffa0b28aa
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-10 16:42:47 +0300

    fix: select only one card cover

--- client/src/features/LobbyPage/components/CardsCover/CardsCover.tsx
+++ client/src/features/LobbyPage/components/CardsCover/CardsCover.tsx
@@ -2,24 +2,31 @@ import { FunctionComponent, useState } from "react";
 
 interface CardsCoverProps {
   imgNumber: number;
+  selected: boolean | null;
+  addInState: (str: string, idx: number) => void;
 }
 
 export const CardsCover: FunctionComponent<CardsCoverProps> = ({
   imgNumber,
+  selected,
+  addInState,
 }): JSX.Element => {
-  const [select, setSelect] = useState<boolean>(false);
+  // const [select, setSelect] = useState<boolean>(false);
 
-  const toggleSelect = () => {
-    setSelect((prev) => !prev);
+  const handlerClick = () => {
+    // setSelect((prev) => !prev);
+    addInState(`./img/cover${imgNumber}.png`, imgNumber);
   };
 
   return (
     <div
       className={
-        select ? "game-cards__cover-item selected" : "game-cards__cover-item"
+        selected ? "game-cards__cover-item selected" : "game-cards__cover-item"
       }
       role="none"
-      onClick={toggleSelect}
+      onClick={() => {
+        handlerClick();
+      }}
     >
       <img src={`./img/cover${imgNumber}.png`} alt="card cover" />
     </div>
