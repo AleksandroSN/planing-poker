4c1fff39f1f91a5ef28108a7b203a6ab405dec2a
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-13 17:13:38 +0300

    refactor: refactor CardWithValue component

--- client/src/features/LobbyPage/components/CardsValue/CardsWithValue.tsx
+++ client/src/features/LobbyPage/components/CardsValue/CardsWithValue.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { searchEnterKey } from "./cardsWithValueHelper";
 
 interface CardsWithValueProps {
   value: string;
@@ -9,22 +10,37 @@ export const CardsWithValue: FunctionComponent<CardsWithValueProps> = ({
   value,
   scoreTypeShort,
 }): JSX.Element => {
-  const [state, setState] = useState<boolean>(true);
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [cardValue, setCardValue] = useState<string>("");
+
+  useEffect(() => {
+    setCardValue(value);
+    return () => {};
+  }, [value]);
 
   const toggleState = (): void => {
-    setState((x) => !x);
+    setEditMode((x) => !x);
   };
+
   return (
     <div className="game-cards__cards-value-item">
       <div className="game-cards__cards-value-item__header text-s">
-        <input
-          type="text"
-          name="cardValue"
-          id="cardValue"
-          className="game-cards__cards-value-item__header-input"
-          value={value}
-          disabled={state}
-        />
+        {editMode ? (
+          <input
+            type="text"
+            name="cardValue"
+            id="cardValue"
+            className="game-cards__cards-value-item__header-input"
+            maxLength={2}
+            placeholder={cardValue}
+            onChange={(evt) => setCardValue(evt.target.value)}
+            onKeyDown={(evt) => searchEnterKey(evt, toggleState)}
+          />
+        ) : (
+          <p className="game-cards__cards-value-item__header-text">
+            {cardValue}
+          </p>
+        )}
         <img
           src="./img/edit.svg"
           alt="edit card icon"
@@ -36,7 +52,7 @@ export const CardsWithValue: FunctionComponent<CardsWithValueProps> = ({
       <div className="game-cards__cards-value-item__body text-xxl text-bold">
         {scoreTypeShort}
       </div>
-      <div className="game-cards__cards-value-item__footer text-s">{value}</div>
+      <p className="game-cards__cards-value-item__footer text-s">{cardValue}</p>
     </div>
   );
 };
