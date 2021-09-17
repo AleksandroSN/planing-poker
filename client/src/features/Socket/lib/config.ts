58bbe48d92c6bc56ebb4ff6347f27c5768992ab3
khronic79 <rustam.ptashinskiy@gmail.com>
2021-09-17 19:41:11 +0300

    feat: start develop socket oi API

---
+++ client/src/features/Socket/lib/config.ts
@@ -0,0 +1,1 @@
+export const IO_SOCKET_HOST = "http://localhost:3030";
---
+++ client/src/features/Socket/types/index.ts
@@ -0,0 +1,13 @@
+export enum SocketActions {
+  CREATE_NEW_ROOM = "create new room",
+  ADD_NEW_TEAM_MEMBER = "add new team member",
+  GET_LOBBY_MEMBERS = "get lobby members",
+  ADD_NEW_ISSUE = "add new issue",
+  RECIEVE_NEW_ISSUE = "recieve new issue",
+  GET_LOBBY_ISSUES = "get lobby issues",
+  GET_CHAT_MESSAGES = "get chat messages",
+  SEND_CHAT_MESSAGE = "send chat message",
+  RECONNECT_TO_LOBBY = "reconnect to lobby",
+  RECIEVE_NEW_MESSAGE = "recieve new message",
+  NOTIFY_ABOUT_NEW_MEMBER = "notify about new member",
+}
