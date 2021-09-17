91731263ed11eec51395dc3b6c854cea18abf06e
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-17 19:34:52 +0300

    fix: clear app and refactor style.css modal component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { FunctionComponent } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
-import { Issues } from "./features/LobbyPage";
 
 export const App: FunctionComponent = (): JSX.Element => {
   return (
