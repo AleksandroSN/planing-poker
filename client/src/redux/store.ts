8a2c1b158f5ba5cbdf5f21645d23e24099f297ce
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-21 18:47:53 +0300

    feat: add devtools in redux

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,4 +1,10 @@
 import { createStore } from "redux";
 import { rootReducer } from "./reducer";
 
-export const store = createStore(rootReducer);
+/* eslint-disable no-underscore-dangle */
+export const store = createStore(
+  rootReducer /* preloadedState, */,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
