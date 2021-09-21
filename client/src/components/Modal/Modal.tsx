cfe09b78ce981f7abf828b444c79f60ed848933f
AleksandroSN <59736420+AleksandroSN@users.noreply.github.com>
2021-09-21 12:36:31 +0300

    refactor: delete conflict string

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,9 +1,5 @@
 import { FunctionComponent } from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD:client/src/components/Modal/Modal.tsx
-=======
-import { Button } from "../Button/Button/Button";
->>>>>>> 7eff4ae976f8109e5a8b808e6eb11c14eeb79a85:client/src/features/MainPage/components/Modal/Modal.tsx
 import "./style.scss";
 
 interface ModalProps {
