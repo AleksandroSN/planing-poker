d70a809379210033cf2497c6c415c59f7b3e7e9a
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-22 14:43:41 +0300

    fix: fix imports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,9 +3,9 @@ import { Chat } from "./Chat";
 import { InputSelect } from "./InputSelect";
 import { InputText } from "./InputText";
 import { Modal } from "./Modal";
-import { Switcher } from "./Switcher/Switcher";
+import { Switcher } from "./Switcher";
 import { Timer } from "./Timer";
-import { User } from "./User/User";
+import { User } from "./User";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
