7fb9e3a39969d4cba35182d7b0ae23da78fa4a2a
Hanna Dziahonskaya <svetlova.hanna@mail.ru>
2021-09-25 23:08:45 +0300

    feat: add copy text button, add cases for content displaying, add adaptivity

--- client/src/features/LobbyPage/components/Layout/Layout.tsx
+++ client/src/features/LobbyPage/components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useLocation } from "react-router";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { GameSettings, Issues } from "..";
-import { InputText } from "../../../../components";
+import { Button, InputText } from "../../../../components";
 import { User } from "../../../../components/User/User";
 import { FormValues } from "../../../../types/interface";
 import { AddCardSection } from "../AddCardSection";
@@ -10,6 +12,8 @@ import "./style.scss";
 
 export const Layout: FunctionComponent = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMaster, setIsMaster] = useState(true);
+  const location = useLocation();
 
   const handleConnect = () => {
     console.log("connect");
@@ -35,14 +39,54 @@ export const Layout: FunctionComponent = (): JSX.Element => {
               isYou
             />
           </div>
-          <div className="">
-            <form className="start-game__form">
-              {/* <InputText defaultValue="link" /> */}
-            </form>
-            <form className="start-game__form">
-              {/* <InputText defaultValue="link" /> */}
-            </form>
-          </div>
+          {isMaster && (
+            <div className="start-game__block">
+              <div className="link__block">
+                <InputText
+                  defaultValue={`${location.pathname}/aasss`}
+                  register={register}
+                  labelText="Link:"
+                  inputClasses="lobby-page-link__input"
+                  labelClasses="lobby-link__label"
+                />
+                <Button
+                  type="button"
+                  onClick={() =>
+                    navigator.clipboard.writeText(`${location.pathname}/aasss`)
+                  }
+                  classes="copy-link__button"
+                >
+                  Copy
+                </Button>
+              </div>
+              <div className="start-game__button-block">
+                <Button
+                  type="submit"
+                  onClick={() => console.log("Redirect to game page")}
+                >
+                  Start Game
+                </Button>
+                <Button
+                  type="button"
+                  onClick={() => console.log("Redirect to main page")}
+                  classes="button-cancel"
+                >
+                  Cancel Game
+                </Button>
+              </div>
+            </div>
+          )}
+          {!isMaster && (
+            <div className="exit-game__block">
+              <Button
+                type="button"
+                onClick={() => console.log("Exit")}
+                classes="button-cancel"
+              >
+                Exit
+              </Button>
+            </div>
+          )}
           <div className="members__block">
             <h2 className="members__title text-xl">Members:</h2>
             <User
@@ -54,19 +98,27 @@ export const Layout: FunctionComponent = (): JSX.Element => {
               isYou={false}
             />
           </div>
-          <div className="issues__block">
-            <Issues />
-          </div>
-          <div className="settings__block">
-            <h2 className="settings__block__title text-xl">Game Settings:</h2>
-            <GameSettings register={register} />
-          </div>
-          <div className="">
-            <CoverSection />
-          </div>
-          <div className="card-values__block">
-            <AddCardSection />
-          </div>
+          {isMaster && (
+            <div className="issues__block">
+              <Issues />
+            </div>
+          )}
+          {isMaster && (
+            <div className="settings__block">
+              <h2 className="settings__block__title text-xl">Game Settings:</h2>
+              <GameSettings register={register} />
+            </div>
+          )}
+          {isMaster && (
+            <div className="">
+              <CoverSection />
+            </div>
+          )}
+          {isMaster && (
+            <div className="card-values__block">
+              <AddCardSection />
+            </div>
+          )}
         </form>
       </div>
     </>
