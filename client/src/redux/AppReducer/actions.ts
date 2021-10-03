0b54b2007e1b50101be183ceada57b063e2b5513
khronic79 <rustam.ptashinskiy@gmail.com>
2021-10-03 23:05:56 +0300

    feat: start kick members

--- client/src/redux/AppReducer/actions.ts
+++ client/src/redux/AppReducer/actions.ts
@@ -2,4 +2,7 @@ export enum AppReducerActions {
   loadData = "IS_LOADING_DATA",
   errorHappend = "IS_ERROR_HAPPEND",
   toggleChatState = "TOGGLE_CHAT",
+  kickVoteStart = "KICK_VOTE_START",
+  kickVoteSuggest = "KICK_VOTE_SUGGEST",
+  kickMemberNotify = "KICK_MEMBER_NOTIFY",
 }
--- client/src/redux/AppReducer/reducer.ts
+++ client/src/redux/AppReducer/reducer.ts
@@ -5,12 +5,18 @@ export type AppState = {
   isLoading: boolean;
   isError: boolean;
   chatOpen: boolean;
+  kickVoteStart: boolean;
+  kickVoteSuggest: boolean;
+  kickMemberNotify: boolean;
 };
 
 const initialAppState: AppState = {
   isLoading: false,
   isError: false,
   chatOpen: false,
+  kickVoteStart: false,
+  kickVoteSuggest: false,
+  kickMemberNotify: false,
 };
 
 export const appReducer = (
@@ -29,6 +35,18 @@ export const appReducer = (
     case AppReducerActions.toggleChatState: {
       const { chatOpen } = action.payload as unknown as AppState;
       return { ...state, chatOpen };
+    }
+    case AppReducerActions.kickVoteStart: {
+      const kickVoteStart = action.payload as unknown as boolean;
+      return { ...state, kickVoteStart };
+    }
+    case AppReducerActions.kickVoteSuggest: {
+      const kickVoteSuggest = action.payload as unknown as boolean;
+      return { ...state, kickVoteSuggest };
+    }
+    case AppReducerActions.kickMemberNotify: {
+      const kickMemberNotify = action.payload as unknown as boolean;
+      return { ...state, kickMemberNotify };
     }
     default:
       return state;
