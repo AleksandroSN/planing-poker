0f668cd4ac6731b350397805d6a4366d237c2b95
Hanna Dziahonskaya <svetlova.hanna@mail.ru>
2021-10-04 18:34:26 +0300

    feat: add export

--- client/src/features/GamePage/components/Layout/Layout.tsx
+++ client/src/features/GamePage/components/Layout/Layout.tsx
@@ -1,15 +1,18 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Issues } from "../../../../components/Issues/Issues";
 import { Button, Chat, Timer } from "../../../../components";
-import { User } from "../../../../components/User/User";
 import { FormValues } from "../../../../types/interface";
-import { AppSettings, useAppSelector } from "../../../../redux/store";
+import {
+  AppSettings,
+  IssuesRedux,
+  useAppSelector,
+} from "../../../../redux/store";
 import { ProgressBar } from "../ProgressBar";
 import { Cards } from "../../../../components/AddCardSection/Cards";
 import "../GamePageView/style.scss";
 import { ScrumMaster } from "../../../../components/ScrumMaster/ScrumMaster";
 import { AnimeChatMount } from "../../../../lib";
+import { Issue } from "../../../../components/Issues/IssueItem/Issue";
 
 // TODO add chat
 export const Layout: FunctionComponent = (): JSX.Element => {
@@ -17,6 +20,28 @@ export const Layout: FunctionComponent = (): JSX.Element => {
   const [isPlayer, setIsPlayer] = useState(true);
   const [isStart, setIsStart] = useState(true);
   const { chatOpen } = useAppSelector(AppSettings);
+  const issuesFromRedux = useAppSelector(IssuesRedux);
+
+  /* useEffect(() => {
+    if (issuesFromRedux.issues) {
+      const activeIssue = issuesFromRedux.issues.filter(
+        (issue) => issue.status === "Active"
+      );
+      setIsActive(activeIssue);
+    }
+  }, [issuesFromRedux]);
+ */
+
+  const issues = issuesFromRedux.issues.map((issue) => {
+    return (
+      <Issue
+        id={issue.id}
+        title={issue.title}
+        link={issue.link}
+        priority={issue.priority}
+      />
+    );
+  });
 
   const handleExit = () => {
     console.log("exit");
@@ -58,9 +83,7 @@ export const Layout: FunctionComponent = (): JSX.Element => {
             )}
           </div>
           <div className="game-issues__wrapper">
-            <div className="game-issues__block">
-              <Issues />
-            </div>
+            <div className="game-issues__block">{issues}</div>
             {isMaster && (
               <div className="game-issues__block">
                 <div className="game-timer__wrapper">
