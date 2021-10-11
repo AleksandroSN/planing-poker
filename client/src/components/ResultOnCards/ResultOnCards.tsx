547d6bdb21cc70e8b2add1073aaa0d16c38ed1ce
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-10-11 14:40:16 +0300

    feat: add render result on game page

--- client/src/components/ResultOnCards/ResultOnCards.tsx
+++ client/src/components/ResultOnCards/ResultOnCards.tsx
@@ -1,17 +1,45 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+import {
+  GameSettingsCurrent,
+  ResultRedux,
+  useAppSelector,
+} from "../../redux/store";
 import { CardsWithStats } from "../CardsWithStats";
-import { cardsMock } from "./cardsMock";
 import "./style.scss";
 
+interface ResultsData {
+  value: string;
+  stat: number;
+}
+
 export const ResultOnCards: FunctionComponent = (): JSX.Element => {
-  const [resultCards, setResultCards] = useState(cardsMock);
+  const [resultCards, setResultCards] = useState<ResultsData[]>([]);
+  const issueResults = useAppSelector(ResultRedux);
+  const { scoreTypeShort } = useAppSelector(GameSettingsCurrent);
+
+  useEffect(() => {
+    const resArr: ResultsData[] = [];
+    Object.keys(issueResults).forEach((issue) => {
+      Object.keys(issueResults[issue].results).forEach((cardValue) => {
+        const resultObj = {
+          value: cardValue,
+          stat: issueResults[issue].results[cardValue],
+        };
+        resArr.push(resultObj);
+      });
+    });
+    setResultCards(resArr);
+  }, [issueResults]);
+
+  // clear after start round
+
   const renderCards = resultCards.map((card) => {
     return (
       <CardsWithStats
         key={card.value}
         value={card.value}
-        scoreTypeShort={card.scoreTypeShort}
-        stats={card.stats}
+        scoreTypeShort={scoreTypeShort}
+        stats={card.stat}
       />
     );
   });
